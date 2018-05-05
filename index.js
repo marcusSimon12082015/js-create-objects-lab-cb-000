31c83556c1f1b88ad17ebe9a221703381d5777ce
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-05-05 13:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,3 +26,10 @@ function Bird(name,breed)
   this.name = name;
   this.breed = breed;
 }
+
+function Fish(name,species,waterConditions)
+{
+  this.name = name;
+  this.species = species;
+  this.waterConditions = waterConditions;
+}
